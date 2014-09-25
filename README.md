a2b18ab7d2acdb77dd3231ace92f44a2fa16e20c
andou <antonio.pastorino@gmail.com>
2014-09-25 10:02:02 +0200

    - Added class documentation - Added LICENSE - Added README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+# Autoloader
+
+A simple class for class autoloading based on classes namespaces
+
+## Installation
+
+Add a dependency on `andou/autoloader` to your project's `composer.json` file if you use [Composer](http://getcomposer.org/) to manage the dependencies of your project.
+You have to also add the relative repository.
+
+Here is a minimal example of a `composer.json` file that just defines a dependency on `andou/autoloader`:
+
+```json
+{
+    "require": {
+        "andou/autoloader": "*"
+    },
+    "repositories": [
+    {
+      "type": "git",
+      "url": "https://github.com/andou/autoloader.git"
+    }
+  ],
+}
+```    
+
+## Usage Examples
+
+```php
+require_once './vendor/autoload.php';
+
+new Andou\Autoloader(__DIR__ . "/path/to/your/classes");
+```
+
--- src/Autoloader.php
+++ src/Autoloader.php
@@ -3,26 +3,56 @@
 namespace Andou;
 
 /**
- * This class is accountable of classes autoloading
+ * Your own personal Autoloader.
+ * 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Antonio Pastorino <antonio.pastorino@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * 
+ * @author Antonio Pastorino <antonio.pastorino@gmail.com>
+ * @category autoloader
+ * @package andou/autoloader
+ * @copyright MIT License (http://opensource.org/licenses/MIT)
  */
 class Autoloader {
 
+  /**
+   * The base directory from which to start atuoloading
+   *
+   * @var string 
+   */
   protected $_base_dir = NULL;
 
+  /**
+   * Class constructor. Sets the base directory for autoloading
+   * 
+   * @param string $base_directory
+   */
   public function __construct($base_directory) {
     $this->_register($base_directory);
   }
 
   /**
-   * Registers autoloading methods
-   */
-  protected function _register($base_directory) {
-    $this->_base_dir = $base_directory;
-    spl_autoload_register(array($this, 'autoload'));
-  }
-
-  /**
-   * Autoloads check classes
+   * Autoloading method based on classname and namespace
    * 
    * @param string $classname
    */
@@ -32,7 +62,17 @@ class Autoloader {
   }
 
   /**
-   * Generic loading by filename
+   * Registers an autoloading method
+   * 
+   * @param string $base_directory
+   */
+  protected function _register($base_directory) {
+    $this->_base_dir = $base_directory;
+    spl_autoload_register(array($this, 'autoload'));
+  }
+
+  /**
+   * Generic loading of classes by filename
    * 
    * @param string $_fl
    */
